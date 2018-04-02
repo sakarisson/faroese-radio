86f1643095d47b29477779cd3155089c449f6767
Kristian Sakarisson <kristian@sakarisson.com>
2018-04-02 18:23:58 +0300

    Implemented kvf fetcher

--- src/parser/parsers.js
+++ src/parser/parsers.js
@@ -5,11 +5,27 @@
  * music from a given radio station
  */
 
+const fetch = require('node-fetch');
+const xmlParser = require('xml2js');
+
 class Parser {
   constructor() {
     this.link = null;
     this.json = null;
   }
+
+  getCurrentData() {
+    return new Promise(async (resolve, reject) => {
+      try {
+        const data = await fetch(this.link);
+        const stringifiedData = await data.text();
+        resolve(stringifiedData);
+      } catch (e) {
+        reject(e);
+      }
+    });
+  }
+
   // eslint-disable-next-line class-methods-use-this
   setJson() {
     throw new Error('getJson has not been implemented');
@@ -21,6 +37,27 @@ class Parser {
   }
 }
 
-module.exports.KvfParser = class KvfParser extends Parser {
+class KvfParser extends Parser {
+  constructor() {
+    super();
+    this.link = 'http://kvf.fo/service/now-next.xml';
+  }
 
-};
+  setJson() {
+    return new Promise(async (resolve, reject) => {
+      const xml = await this.getCurrentData();
+      xmlParser.parseString(xml, (err, result) => {
+        if (err) {
+          reject(err);
+        }
+        this.json = result;
+        resolve();
+      });
+    });
+  }
+
+  async getCurrentSong() {
+    await this.setJson();
+    return this.json.data.now;
+  }
+}
