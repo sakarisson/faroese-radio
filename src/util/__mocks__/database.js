2fdcde3511a4942e24cb62ca4c85d7ac649da7f6
Kristian Sakarisson <kristian@sakarisson.com>
2018-04-05 14:34:42 +0300

    Updated database to be much cleaner and independent

--- src/util/__mocks__/database.js
+++ src/util/__mocks__/database.js
@@ -6,31 +6,40 @@ const mockDatabase = {
     short_name: 'kvf',
     long_name: 'Kringvarp Føroya',
   }],
+  artists: [{
+    id: 1,
+    name: 'kvf Last Artist',
+  }],
   songs: [{
     id: 1,
-    artist: 'Random',
-    title: 'Some song',
+    title: 'kvf Last Song',
+    fk_artists: 1,
+  }],
+  song_plays: [{
+    id: 1,
+    fk_songs: 1,
     fk_stations: 1,
     time_played: '2018-04-04 14:44:09.134784',
-  },
-  {
-    id: 2,
-    artist: 'kvf Last Artist',
-    title: 'kvf Last Song',
-    fk_stations: 1,
-    time_played: '2018-04-04 16:20:04.387387',
   }],
 };
 
 export const getLastStationSong = async (shortName) => {
-  const { stations, songs } = mockDatabase;
+  const {
+    // eslint-disable-next-line camelcase
+    stations, artists, songs, song_plays,
+  } = mockDatabase;
   const stationId = _.where(stations, { short_name: shortName })[0].id;
-  const stationSongs = _.where(songs, { fk_stations: stationId });
-  const lastStationSong = _.max(stationSongs, song => song.id);
-  return {
-    artist: lastStationSong.artist,
+  const stationSongsPlays = _.where(song_plays, { fk_stations: stationId });
+  const lastStationSongPlay = _.max(stationSongsPlays, song => song.id);
+  const lastStationSong = _.findWhere(songs, { id: lastStationSongPlay.fk_songs });
+  const lastStationArtist = _.findWhere(artists, { id: lastStationSong.fk_artists });
+
+  const song = {
+    artist: lastStationArtist.name,
     title: lastStationSong.title,
   };
+
+  return song;
 };
 
 export default null;
--- src/util/database.js
+++ src/util/database.js
@@ -38,11 +38,13 @@ export const getStationId = async (shortName) => {
 export const getLastStationSong = async (shortName) => {
   try {
     const result = await client.query(`
-      select songs.artist, songs.title
-      from songs, stations
-      where songs.fk_stations = stations.id
-      and stations.short_name = $1
-      order by songs.id desc
+      select artists.name, songs.title
+      from artists, songs, stations, song_plays
+      where stations.short_name = $1
+      and song_plays.fk_stations = stations.id
+      and song_plays.fk_songs = songs.id
+      and songs.fk_artists = artists.id
+      order by song_plays.id desc
       limit 1`, [shortName]);
     const { artist, title } = result.rows[0];
     return { artist, title };
@@ -53,18 +55,115 @@ export const getLastStationSong = async (shortName) => {
   return null;
 };
 
-export const insertSong = async (song) => {
+const getSongId = async (song) => {
   try {
-    const stationId = await getStationId(song.station);
-    await client.query(`
-      insert into songs (artist, title, fk_stations, time_played)
-      values ($1, $2, $3, now())
+    const result = await client.query(`
+      select songs.id from songs, artists
+      where songs.fk_artists = artists.id
+      and artists.name = $1
+      and songs.title = $2
     `, [
       song.artist,
       song.title,
+    ]);
+
+    if (result.rows.length > 0) {
+      const { id } = result.rows[0];
+      return id;
+    }
+  } catch (e) {
+    throw e;
+  }
+  return null;
+};
+
+const getArtistId = async (artist) => {
+  try {
+    const result = await client.query(`
+      select artists.id from artists
+      where artists.name = $1
+    `, [
+      artist,
+    ]);
+
+    if (result.rows.length > 0) {
+      const { id } = result.rows[0];
+      return id;
+    }
+  } catch (e) {
+    throw e;
+  }
+  return null;
+};
+
+const addArtistToDatabase = async (artist) => {
+  try {
+    const result = await client.query(`
+      insert into artists
+      (name)
+      values
+      ($1)
+      returning id
+    `, [
+      artist,
+    ]);
+
+    if (result.rows.length > 0) {
+      const { id } = result.rows[0];
+      return id;
+    }
+  } catch (e) {
+    throw e;
+  }
+  return null;
+};
+
+const addSongToDatabase = async (song) => {
+  try {
+    let artistId = await getArtistId(song.artist);
+    if (artistId === null) {
+      artistId = await addArtistToDatabase(song.artist);
+    }
+    const result = await client.query(`
+      insert into songs
+      (title, fk_artists)
+      values
+      ($1, $2)
+      returning id
+    `, [
+      song.title,
+      artistId,
+    ]);
+
+    if (result.rows.length > 0) {
+      const { id } = result.rows[0];
+      return id;
+    }
+  } catch (e) {
+    throw e;
+  }
+  return null;
+};
+
+export const insertSong = async (song) => {
+  try {
+    let songId = await getSongId(song);
+    if (songId === null) {
+      songId = await addSongToDatabase(song);
+    }
+    const stationId = await getStationId(song.station);
+    await client.query(`
+      insert into song_plays
+      (fk_songs, fk_stations)
+      values
+      ($1, $2)
+    `, [
+      songId,
       stationId,
     ]);
   } catch (e) {
+    // eslint-disable-next-line no-console
+    console.log(e);
     return false;
   }
   return false;
