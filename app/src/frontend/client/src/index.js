2e3e472c4978cb4d19f7521307744f02eb04b7e0
Kristian Sakarisson <kristian@sakarisson.com>
2018-04-11 14:02:32 +0300

    Fixed eslint for react

--- app/src/frontend/client/src/index.js
+++ app/src/frontend/client/src/index.js
@@ -4,5 +4,6 @@ import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
+// eslint-disable-next-line no-undef
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
