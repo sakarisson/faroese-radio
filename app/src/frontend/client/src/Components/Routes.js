5ebacceecdc5584eeb6046d95f173c56cab7e624
Kristian Sakarisson <kristian@sakarisson.com>
2018-04-16 15:57:54 +0300

    Refactored components out of index

---
+++ app/src/frontend/client/src/Components/Routes.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import {
+  BrowserRouter as Router,
+  Route,
+  Switch,
+} from 'react-router-dom';
+import PropTypes from 'prop-types';
+import ArtistRoutes from '../Helpers/GenerateArtistRoutes';
+import Home from './Home';
+
+const Routes = props => (
+  <div className="routesContainer">
+    <Router>
+      <Switch>
+        <Route exact path="/" render={() => <Home store={props.store} />} />
+        {ArtistRoutes(props.store)}
+      </Switch>
+    </Router>
+  </div>
+);
+
+Routes.propTypes = {
+  store: PropTypes.shape({ getState: PropTypes.func.isRequired }).isRequired,
+};
+
+export default Routes;
--- app/src/frontend/client/src/index.js
+++ app/src/frontend/client/src/index.js
@@ -1,34 +1,12 @@
 import React from 'react';
 import { render } from 'react-dom';
-import {
-  BrowserRouter as Router,
-  Route,
-  Switch,
-} from 'react-router-dom';
 import { createStore } from 'redux';
-import PropTypes from 'prop-types';
-import Home from './Components/Home';
 import registerServiceWorker from './registerServiceWorker';
 import dataStore from './Reducers';
-import ArtistRoutes from './Helpers/GenerateArtistRoutes';
 import { addArtists } from './Actions/Artists';
+import App from './Components/App';
 
 const store = createStore(dataStore);
-
-const App = props => (
-  <Router>
-    <div className="container">
-      <Switch>
-        <Route exact path="/" render={() => <Home store={props.store} />} />
-        {ArtistRoutes(props.store)}
-      </Switch>
-    </div>
-  </Router>
-);
-
-App.propTypes = {
-  store: PropTypes.shape({ getState: PropTypes.func.isRequired }).isRequired,
-};
 
 const renderApp = () => render(<App store={store} />, document.getElementById('root'));
 
